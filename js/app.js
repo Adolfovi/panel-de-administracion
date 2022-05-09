9710478a39c317259dc850ac74ddd3ef5db21d27
Adolfovi <dolfillo99@gmail.com>
2022-05-09 14:09:47 +0200

    Instalación de ckeditor5

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
-document.getElementById("nav-icon3").addEventListener("click", function(){
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+
+document.getElementById("nav-icon3").addEventListener("click", function () {
     document.getElementById("nav-icon3").classList.toggle("open");
 });
+
+
+ClassicEditor.create(document.querySelector('.ckeditor'))
+    .then(editor => {
+        window.editor = editor;
+    })
+    .catch(error => {
+        console.error('There was a problem initializing the editor.', error);
+    });
+
+
+    
